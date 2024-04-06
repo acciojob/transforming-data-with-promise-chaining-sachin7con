01eb13d2fc3c4eb6402ec0cab60ab662817450a7
sachin7con <86023048+sachin7con@users.noreply.github.com>
2024-04-06 18:16:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-//your JS code here. If required.
+//SGN your JS code here. If required.
+document.getElementById("btn").onclick = function() {myFunction()};
+
+function myFunction() {
+ let input = document.getElementById("ip").value;
+	let output = document.getElementById("output");
+
+	let promise1= new Promise((resolve, reject) => { 
+		resolve(output.innerHTML = 'Result: ${input}');
+		return promise2;
+        
+		},2000).then((data1) =>{
+		resolve(output.innerHTML = 'Result: {data1 * 2}');
+		},)
+												   
+												   											   
+	}
